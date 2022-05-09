e124aa4eda8f4ef649637ab1fc5773399a684687
Michael Lu <lu.michael@outlook.com>
2022-05-09 11:25:26 -0700

    backup

--- atu/helper_oc_controller.py
+++ atu/helper_oc_controller.py
@@ -1,50 +1,34 @@
-# %%
 import matlab.engine
 import numpy as np
+import pathlib
+import os
 
 
 class HelperOCController:
     def __init__(self):
         print("connecting to matlab engine")
+        self.helperOC_path = os.path.join(pathlib.Path().resolve(), "matlab/helperOC")
         self.eng = matlab.engine.start_matlab()
+        self.eng.addpath(self.eng.genpath(self.helperOC_path))
         self.eng.addpath(
-            self.eng.genpath(r"/local-scratch/localhome/mla233/hj/Pendulum/Pendulum")
-        )
-        self.eng.addpath(
-            self.eng.genpath(r"/local-scratch/localhome/mla233/hj/ToolboxLS")
+            self.eng.genpath(os.path.join(pathlib.Path().resolve(), "matlab/ToolboxLS"))
         )
 
-        # self.vars = self.eng.eval(
-        #     "load('/local-scratch/localhome/mla233/Downloads/Pendulum/Pendulum/pend_brt.mat')"
-        # )
-        self.vars = self.eng.eval(
-            "load('/local-scratch/localhome/mla233/hj/Pendulum/Pendulum/pend_brt_max_min_2.mat')"
-        )
-        self.vars_brt_max = self.eng.eval(
-            "load('/local-scratch/localhome/mla233/hj/Pendulum/Pendulum/pend_brt_home_2.mat')"
-        )
-        # print("done 2")
+        self.vars = self.eng.eval(f"load('{self.helperOC_path}/pend_brt_min_max.mat')")
+        print("connected to matlab engine")
 
     def opt_ctrl_value(self, state):
-        # state[0] = th, state[1] = thdot
         x = matlab.double([[state[0]], [state[1]]])
         opt_ctrl, value = self.eng.pendulum_opt_ctrl(x, self.vars, nargout=2)
-        # opt_ctrl, value = self.eng.pendulum_opt_ctrl(x, nargout=2)
         return np.array([opt_ctrl]), value
 
     def safe_ctrl_bnds(self, state):
-        # state[0] = th, state[1] = thdot
         x = matlab.double([[state[0]], [state[1]]])
-        low, high = self.eng.pendulum_safe_ctrl_bnds(x, self.vars_brt_max, nargout=2)
+        low, high = self.eng.pendulum_safe_ctrl_bnds(x, self.vars, nargout=2)
         return low, high
 
 
-# %%
 if __name__ in "__main__":
     c = HelperOCController()
-    # print(c.opt_ctrl_value([0, 0]))
+    print(c.opt_ctrl_value([0, 0]))
     print(c.safe_ctrl_bnds([0, 0]))
-    # c.opt_ctrl_value([-0.0875, -0.9786])
-
-
-# %%
