67426ee73d4f438313d14b68a39b6423b82b0b4b
Michael Lu <lu.michael@outlook.com>
2022-10-22 21:01:55 -0700

    fixed

--- atu/envs/reach_avoid.py
+++ atu/envs/reach_avoid.py
@@ -34,7 +34,9 @@ class ReachAvoid3DEnv(gym.Env):
         self.brt = np.load(os.path.join(dir_path, "assets/brts/air3d_brt_test.npy"))
         self.car = car_brt
         self.dt = 0.05
-        self.action_space = Box(low=-1 * self.car.we_max, high=self.car.we_max, dtype=np.float32, shape=(1,))
+        self.action_space = Box(
+            low=-1 * self.car.we_max, high=self.car.we_max, dtype=np.float32, shape=(1,)
+        )
         self.world_boundary = np.array([4.5, 4.5, np.pi], dtype=np.float32)
 
         self.observation_space = Box(
@@ -55,27 +57,21 @@ class ReachAvoid3DEnv(gym.Env):
         self.grid = grid
         self.fig, self.ax = plt.subplots(figsize=(5, 5))
 
-        # yes
-        self.evader_state = np.array([0, 0, 0])
-        self.persuer_state = np.array([2, 0, np.pi])
-        print(self.relative_state(self.persuer_state, self.evader_state))
-        index = self.grid.get_index(self.relative_state(self.persuer_state, self.evader_state))
-        print(self.grid.grid_points[0][index[0]], self.grid.grid_points[1][index[1]], self.grid.grid_points[2][index[2]])
-        
-        # no
-        self.evader_state = np.array([0, 0, -np.pi])
-        self.persuer_state = np.array([-2, 0, 0])
-        print(self.relative_state(self.persuer_state, self.evader_state))
-        index = self.grid.get_index(self.relative_state(self.persuer_state, self.evader_state))
-        print(self.grid.grid_points[0][index[0]], self.grid.grid_points[1][index[1]], self.grid.grid_points[2][index[2]])
+        # if theta_e == 0, does everything work?
+        # self.evader_state = np.array([1, 1, 0])
+        # self.persuer_state = np.array([2, -2, 3 * np.pi / 4])
 
-        self.evader_state = np.array([0, 0, np.pi/2])
-        self.persuer_state = np.array([-2, 0, 0])
-        print(self.relative_state(self.persuer_state, self.evader_state))
-        index = self.grid.get_index(self.relative_state(self.persuer_state, self.evader_state))
-
-        exit()
-        
+        self.evader_state = np.array([0, 0, -np.pi / 2])
+        self.persuer_state = np.array([2, 0, -np.pi / 2])
+        # print(self.relative_state(self.persuer_state, self.evader_state))
+        # index = self.grid.get_index(
+        #     self.relative_state(self.persuer_state, self.evader_state)
+        # )
+        # print(
+        #     self.grid.grid_points[0][index[0]],
+        #     self.grid.grid_points[1][index[1]],
+        #     self.grid.grid_points[2][index[2]],
+        # )
 
         self.goal_location = np.array([2, 2, 0.2])
 
@@ -89,36 +85,42 @@ class ReachAvoid3DEnv(gym.Env):
         # self.evader_state = self.car.dynamics_non_hcl(0, self.evader_state, opt_ctrl, is_evader=True) * self.dt + self.evader_state
         # self.evader_state[2] = self.normalize_angle(self.evader_state[2])
         opt_dstb = self.opt_dstb()
-        self.persuer_state = self.car.dynamics_non_hcl(0, self.persuer_state, opt_dstb, is_evader=False) * self.dt + self.persuer_state
+        self.persuer_state = (
+            self.car.dynamics_non_hcl(0, self.persuer_state, opt_dstb, is_evader=False)
+            * self.dt
+            + self.persuer_state
+        )
         self.persuer_state[2] = self.normalize_angle(self.persuer_state[2])
 
-        print(f"{self.evader_state=}")
-        print(f"{self.persuer_state=}")
-        print(f"{self.relative_state(self.persuer_state, self.evader_state)}")
         done = False
 
-        if np.linalg.norm(self.relative_state(self.persuer_state, self.evader_state)[:2]) <= cylinder_r:
-            print('capture')
+        if (
+            np.linalg.norm(
+                self.relative_state(self.persuer_state, self.evader_state)[:2]
+            )
+            <= cylinder_r
+        ):
+            print("capture")
             done = True
 
         relative_state = self.relative_state(self.persuer_state, self.evader_state)
-        # print(f"rel state: {relative_state}, {np.linalg.norm(relative_state)}")
-        index = self.grid.get_index(relative_state)
-        # print(f'value: = {self.brt[index]}')
 
-        return {
-            'persuer_state': np.copy(self.persuer_state),
-            'evader_state': np.copy(self.evader_state),
-            'goal_location': np.copy(self.goal_location)
-        }, 0, done, {}
-        
+        return (
+            {
+                "persuer_state": np.copy(self.persuer_state),
+                "evader_state": np.copy(self.evader_state),
+                "goal_location": np.copy(self.goal_location),
+            },
+            0,
+            done,
+            {},
+        )
 
-    def render(self, mode='human'):
+    def render(self, mode="human"):
         self.ax.clear()
-        def add_robot(state, color='green'):
-            self.ax.add_patch(
-                plt.Circle(state[:2], radius=self.car.r, color=color)
-            )
+
+        def add_robot(state, color="green"):
+            self.ax.add_patch(plt.Circle(state[:2], radius=self.car.r, color=color))
 
             dir = state[:2] + self.car.r * np.array(
                 [np.cos(state[2]), np.sin(state[2])]
@@ -126,34 +128,41 @@ class ReachAvoid3DEnv(gym.Env):
 
             self.ax.plot([state[0], dir[0]], [state[1], dir[1]], color="c")
 
-        add_robot(self.evader_state, color='blue')
-        add_robot(self.persuer_state, color='red')
+        add_robot(self.evader_state, color="blue")
+        add_robot(self.persuer_state, color="red")
 
         goal = plt.Circle(
             self.goal_location[:2], radius=self.goal_location[2], color="g"
         )
         self.ax.add_patch(goal)
 
-        
         relative_state = self.relative_state(self.persuer_state, self.evader_state)
         relative_state[2] = self.normalize_angle(relative_state[2])
 
         index = self.grid.get_index(relative_state)
-        print(self.grid.grid_points[0][index[0]], self.grid.grid_points[1][index[1]], self.grid.grid_points[2][index[2]])
         X, Y = np.meshgrid(
             np.linspace(self.grid.min[0], self.grid.max[0], self.grid.pts_each_dim[0]),
             np.linspace(self.grid.min[1], self.grid.max[1], self.grid.pts_each_dim[1]),
-            indexing='ij'
+            indexing="ij",
         )
 
+        # rotate world by -evader_state[2]
+        # angle = -self.evader_state[2]
+        # Xr = X * np.cos(angle) - Y * np.sin(angle)
+        # Yr = X * np.sin(angle) + Y * np.cos(angle)
+
+        # self.ax.contour(
+        #     Xr + self.evader_state[0],
+        #     Yr + self.evader_state[1],
+        #     self.brt[:, :, index[2]],
+        #     levels=[0.2],
+        # )
         self.ax.contour(
             X + self.evader_state[0],
             Y + self.evader_state[1],
             self.brt[:, :, index[2]],
             levels=[0.2],
         )
-
-        
 
         # walls
         # self.ax.hlines(y=[-4.5, 4.5], xmin=[-4.5, -4.5], xmax=[4.5, 4.5], color="k")
@@ -170,7 +179,6 @@ class ReachAvoid3DEnv(gym.Env):
         img = np.frombuffer(self.fig.canvas.tostring_rgb(), dtype=np.uint8)
         img = img.reshape(self.fig.canvas.get_width_height()[::-1] + (3,))
 
-
         if mode == "human":
             self.fig.canvas.flush_events()
             plt.pause(1 / self.metadata["render_fps"])
@@ -186,9 +194,21 @@ class ReachAvoid3DEnv(gym.Env):
         return ((-theta + np.pi) % (2.0 * np.pi) - np.pi) * -1.0
 
     def relative_state(self, persuer_state, evader_state):
+        # brt assume that evader_state is at theta=0
+        rotated_relative_state = np.zeros(3)
         relative_state = persuer_state - evader_state
-        relative_state[2] = self.normalize_angle(relative_state[2])
-        return relative_state
+        # rotate relative state (x, y) by -evader_state[2] to make it equal too when evader_state[2] == 0
+        print(
+            f"{relative_state[2]=}, {np.cos(-evader_state[2])=}, {np.sin(-evader_state[2])=}"
+        )
+        angle = -evader_state[2]
+        # fmt: off
+        rotated_relative_state[0] = relative_state[0] * np.cos(angle) - relative_state[1] * np.sin(angle)
+        rotated_relative_state[1] = relative_state[0] * np.sin(angle) + relative_state[1] * np.cos(angle)
+        # fmt: on
+        # after rotating by -evader_state[2], the relative angle will still be the same
+        rotated_relative_state[2] = self.normalize_angle(relative_state[2])
+        return rotated_relative_state
 
     def opt_dstb(self):
         relative_state = self.relative_state(self.persuer_state, self.evader_state)
@@ -196,7 +216,7 @@ class ReachAvoid3DEnv(gym.Env):
         spat_deriv = spa_deriv(index, self.brt, self.grid)
         opt_dstb = self.car.opt_dstb_non_hcl(spat_deriv)
         print(f"spa deriv: {spat_deriv[2]}, {opt_dstb}")
-        
+
         return opt_dstb
 
     def opt_ctrl(self):
@@ -206,14 +226,16 @@ class ReachAvoid3DEnv(gym.Env):
         opt_dstb = self.car.opt_ctrl_non_hcl(relative_state, spat_deriv)
         return opt_dstb
 
+
 if __name__ in "__main__":
     env = ReachAvoid3DEnv()
     env.render()
     import time
+
     for _ in range(200):
         action = env.action_space.sample()
         obs, reward, done, info = env.step(action)
         if done:
-            print('done')
+            print("done")
             break
         env.render()
